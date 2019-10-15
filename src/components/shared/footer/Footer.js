dd19d3a6b7e9f93a862bcc551885c2694272c98f
bagdat <bagdon.zhumagazieva@mail.ru>
2019-10-15 18:56:29 +0600

    bva

---
+++ src/components/shared/footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+import './Footer.css';
+
+export default function Header(){
+    return(
+        <div className="Footer">
+            <div className="footer-contain">
+                <div className="footer-">
+
+                </div>
+            </div>
+        </div>
+    );
+}
