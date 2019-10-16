62e610ff84632d91127f522c4889842e9c5bbcd8
kbeksultan <bkakpanbay@gmail.com>
2019-10-16 12:02:41 +0600

    main

--- src/app/main/MainLayout.js
+++ src/app/main/MainLayout.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Switch,Route} from 'react-router-dom';
 
 
-import Home from '../../components/pages/Home/Home';
+import Home from '../../components/pages/home/Home';
 
 export default function MainLayout(){
     return(
