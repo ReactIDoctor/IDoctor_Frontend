d5c5abc7480b35dfa9b944fb6d7934c87fff0f0e
bagdat <bagdon.zhumagazieva@mail.ru>
2019-10-16 14:34:08 +0600

    feat: component auth

--- src/app/login/LoginLayout.js
+++ src/app/login/LoginLayout.js
@@ -12,8 +12,54 @@ export default class LoginLayout extends Component{
                     <div className="auth--top__block">
                         <img className="auth-background" src={desktop} alt="background" />
                         <img className="auth-img" src={auth_img} alt="background-auth"/>
-
+                        <div className="auth--title"> Авторизация<br/>
+                            iDoctor.kz</div>
                     </div>
+                </div>
+                <div className="pages--fix__content">
+                    <form className="form--inline-label">
+                        <div className="form--item">
+                            <label>Ваш телефон:</label>
+                            <div className="vue-tel-input">
+                                <select className="form-select">
+                                    <option>KZ</option>
+                                    <option>RUSS</option>
+                                    <option>ENG</option>
+                                </select>
+                                <input type="text" className="form--input" />
+                            </div>
+                        </div>
+                        <div className="form--item">
+                            <label>Ваш пароль:</label>
+                            <div className="vue-tel-input">
+                                <input type="password" className="form--input--pass" />
+                            </div>
+                        </div>
+                        <div className="form--item">
+                            <div className="vue-tel-input">
+                                <button className="button btn-login">Войти</button>
+                            </div>
+                        </div>
+                        <div className="form--item">
+                            <div className="vue-tel-input">
+                                <button className="button btn-forget">не понмю пароль</button>
+                            </div>
+                        </div>
+                        <div className="form--item to_reg">
+                            Не загеристрированы?
+                            <a href="/register">Регистрация</a>
+                        </div>
+                        <div className="footer__copyright-text">
+                            <p>
+                                iDoctor - сервис по поиску врачей о всему Казахстану. Вызов врача на дом,
+                                онлайн-консультация, бесплатная запись на прием
+                            </p>
+                            <p>
+                                ©2013–2019 ТОО “iDoctor.kz”
+                            </p>
+                        </div>
+
+                    </form>
                 </div>
 
 
